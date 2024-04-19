679e9c3ed15758b88b1f7d8b44f03cd38ede026e
Sorawit <sorawit12535@gmail.com>
2024-04-19 15:21:47 +0700

    feat: login template

--- client/src/pages/login/LoginPage.tsx
+++ client/src/pages/login/LoginPage.tsx
@@ -1,14 +1,43 @@
+import { useState } from "react"
 import ThemeButton from "../../components/etc/ThemeButton"
-import { useTheme } from "../../lib/contexts/ThemeContext"
 
 const LoginPage = () => {
-    const {setTheme} = useTheme()
+
+  const [username, setUsername] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+
+  const [isError, setError] = useState<boolean>(false)
 
   return (
     <div className="full-page relative">
         <div className="w-80 flex flex-col item-center">
-            <h3>Sign In</h3>
-            <button onClick={() => setTheme((theme) => (theme === "light") ? "dark" : "light")}>lol</button>
+            <form className="w-full flex flex-col items-center gap-3" onSubmit={(e) => {e.preventDefault()}}>
+              <h2>Sign In</h2>
+              {
+                  isError && <div className="flex w-full justify-center text-red-700">Email or password is wrong!</div>
+              }
+              <div className="w-full flex flex-col gap-2">
+                  <label className="text-base">Username</label>
+                  <input 
+                      type="text" 
+                      placeholder="Email..." 
+                      className="primary-input"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}/>
+              </div>
+              <div className="w-full flex flex-col gap-2">
+                  <label className="text-base">Password</label>
+                  <input 
+                      type="password" 
+                      placeholder="Password..." 
+                      className="primary-input"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}/>
+              </div>
+              {/* <span>Don't have an account? <Link to="/register">Sign up</Link> now!</span> */}
+              <button type="submit" className="primary-button">Sign In</button>
+            </form>
+          
         </div>
         <div className="absolute top-5 right-5">
           <ThemeButton />
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,9 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [],
+  },
+  plugins: [require("daisyui")],
 }
 
