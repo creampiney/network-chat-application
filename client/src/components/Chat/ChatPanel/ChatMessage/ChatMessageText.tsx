72d0b96a01a34f183313271f4025cfc9dab68800
Sorawit <sorawit12535@gmail.com>
2024-04-21 19:47:51 +0700

    fix: chat bubble color

--- client/src/components/Chat/ChatPanel/ChatMessage/ChatMessageText.tsx
+++ client/src/components/Chat/ChatPanel/ChatMessage/ChatMessageText.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ChatMessageText = ({ text }: { text: string }) => {
-  return <div className="chat-bubble bg-indigo-50 break-words">{text}</div>;
+  return <div className="chat-bubble bg-slate-200 dark:bg-slate-700 secondary-text break-words">{text}</div>;
 };
 
 export default ChatMessageText;
