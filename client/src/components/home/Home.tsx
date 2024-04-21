5d2a2d0889ec33ae16fa56f9496714e7d903058f
Sorawit <sorawit12535@gmail.com>
2024-04-21 15:45:10 +0700

    fix: typo

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -4,6 +4,7 @@ import { Avatar } from "@mui/material";
 import { useUser } from "../../lib/contexts/UserContext";
 import { createChat } from "../../lib/chat";
 import LoadingPage from "../../pages/etc/LoadingPage";
+import { IoChatbubbleEllipses } from "react-icons/io5";
 
 const Home = () => {
   const { userCount, userMap } = useOutletContext<{
@@ -21,34 +22,36 @@ const Home = () => {
 
     navigate("/chat/private/" + chatId, { relative: "route" });
   }
+
   if (!currentUser) <LoadingPage />;
 
   return (
     <div className="w-full py-5 px-6 page">
       <div className="flex flex-col w-full">
-        <h2>Online Users Count : {userCount}</h2>
+        <h2>Online Users</h2>
+        <span className="text-sm">{userCount} user{(userCount > 1) ? "s" : ""} online</span>
         <ul className="flex flex-col p-2">
           {userMap.map((user) => {
             return (
               <li
                 key={user.userId}
-                className="flex justify-between hover:bg-slate-300 py-2 dark:hover:bg-slate-700"
+                className="flex justify-between items-center hover:bg-slate-200 dark:hover:bg-slate-700 py-2 px-3 transition-colors"
               >
                 <div className={"flex items-center gap-5"}>
                   <Avatar
                     src={user.avatar}
                     sx={{ width: "48px", height: "48px" }}
                   />
-                  <p className="text-base-100">{user.displayName}</p>
+                  <p className="text-base-100 font-bold">{user.displayName}{(currentUser && user.userId === currentUser.id) && <span className="font-normal"> (me)</span>}</p>
                 </div>
                 {currentUser?.id != user.userId && (
                   <button
-                    className="primary-button"
+                    className="p-2 rounded-full aspect-square hover:bg-slate-100 hover:dark:bg-slate-800 transition-colors"
                     onClick={() => {
                       handleCreateChat(user.userId);
                     }}
                   >
-                    Chat with him/her
+                    <IoChatbubbleEllipses className="w-5 h-5" />
                   </button>
                 )}
               </li>
